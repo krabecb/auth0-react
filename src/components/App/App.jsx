617faf41478633aa9e3918623c5879d8af93f14a
krabecb <briankrabec@gmail.com>
2023-12-18 09:05:06 -0600

    Login works

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Nav from '../Nav/Nav'
 
 function App() {
   return (
     <div className='App'>
       <h1>Auth0 Starter!</h1>
+      <Nav />
     </div>
   )
 }
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import LoginButton from "../Auth/LoginButton";
+
+const Nav = () => (
+    <nav className="Nav">
+      <div>
+        <LoginButton />
+      </div>
+    </nav>
+  );
+
+export default Nav
